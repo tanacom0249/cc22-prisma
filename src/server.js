6ecfc4079c41693261b5d9626b9462d3f27d7c11
tanacom0249 <tanacom0249@hotmail.co.th>
2026-02-25 17:26:48 +0700

    cc22-prisma-App6

--- src/server.js
+++ src/server.js
@@ -1,15 +1,50 @@
-import express from 'express';
+import { skip } from "@prisma/client/runtime/client";
+import prisma from "./prismaClient.js";
+import express from "express";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Welcome Api");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const user = await prisma.user.findUnique({
+    where: { id },
+    include: { posts: true },
+  });
+  res.json(user);
+});
+
+app.post("/users", async (req, res) => {
+  const { email, name, age } = req.body;
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      name,
+      age,
+    },
+  });
+  res.json({
+    message: "Created",
+    newUser,
+  });
+});
+
+app.get("/posts", async (req, res) => {
+  const posts = await prisma.post.findMany();
+  res.json(posts);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
